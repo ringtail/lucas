5d7f719b89f021537969164c46960e91a8eb9b07
ringtail <zhongwei.lzw@alibaba-inc.com>
2017-12-18 01:12:06 +0800

    add tree view

--- frontend/index.go
+++ frontend/index.go
@@ -8,13 +8,16 @@ const HOME_PAGE = `
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<script src="https://cdn.jsdelivr.net/npm/vue@2.5.11/dist/vue.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular.min.js"></script>
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="http://localhost:8081/static/index.js"></script>
 	<link type="text/css" href="http://localhost:8081/static/index.css"/>
     <title>Document</title>
 </head>
 <body ng-app="app">
+	<div ng-controller="lucas">
+		<tree-view nodes="nodes.Nodes"></tree-view>
+	</div>
 </body>
 </html>
 `
--- frontend/static/index.js
+++ frontend/static/index.js
@@ -1,3 +1,36 @@
-$(document).ready(function () {
+var app = angular.module("app", []);
+app.controller('lucas', ['$scope', '$http', function ($scope, $http) {
+    $http({
+        method: 'GET',
+        url: "/store"
+    }).success(function (data) {
+        $scope.nodes = data;
+    })
+}]);
 
-});
+app.directive("treeView", ["$compile", function ($compile) {
+    function link(scope, element, attrs) {
+        scope.hasNodes = function (nodes) {
+            if (nodes != undefined && Object.keys(nodes).length > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+        element.append("<div ng-repeat='item in nodes'>" +
+            "{{ item.RootPath}}" +
+            "<span ng-if='item.KV != undefine'>: {{item.KV.value}}</span>" +
+            "<div ng-if='hasNodes(item.Nodes)' style='margin-left:6px'><tree-view nodes='item.Nodes'></tree-view></div>" +
+            "</div>"
+        );
+        $compile(element.contents())(scope);
+    }
+
+    return {
+        restrict: 'AE',
+        scope: {
+            "nodes": "="
+        },
+        link: link
+    };
+}]);
